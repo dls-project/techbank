4102d8611c92e99e8e7932d4a9c1848f96c2610e
s-cho <s.cho.seraku@gmail.com>
2018-12-13 16:06:19 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+
+module.exports = {
+  env: {
+    FRONT_API_URL: process.env.FRONT_API_URL
+  },
+  srcDir: "./nuxtjs",
+  modules: [
+    '@nuxtjs/proxy',
+  ],
+  proxy: {
+    '/api': 'http://localhost:8000'
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'techbank',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
