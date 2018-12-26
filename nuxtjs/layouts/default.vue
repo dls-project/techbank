2cc68e55824177d116747e30c6afd8984dbe4321
s-cho <s.cho.seraku@gmail.com>
2018-12-27 07:38:00 +0900

    サイドメニュー共通化

--- nuxtjs/layouts/default.vue
+++ nuxtjs/layouts/default.vue
@@ -14,45 +14,25 @@
       </div>
     </header>
     <div class="wrapper">
-      <nav class="menu-bar" v-show="show">
-        <b-list-group>
-          <b-list-group-item v-for="item in links" :key="item.id">
-            <b-link 
-              variant="primary"
-              :to="item.link"
-              :disabled="item.disabled">
-              {{item.title}}
-            </b-link>{{item.link}}
-            <!-- <b-progress
-              :variant="item.variant"
-              :value="item.sts"
-              :max="max"
-              height="6px"
-              animated
-            /> -->
-          </b-list-group-item>
-        </b-list-group>
-      </nav>
+      <div class="left-side" v-show="show">
+        <org-menu/>
+      </div>
       <div class="main">
         <nuxt/>
       </div>
     </div>
   </div>
 </template>
+
 <script>
+import OrgMenu from '@/components/organisms/OrgMenu'
 export default {
+  components: {
+    OrgMenu,
+  },
   data() {
     return {
       show: true,
-      links:[
-        {id:0, title:'認証機能', link:'/login', disabled:false, sts:40, variant:'danger'},
-        {id:1, title:'HOME', link:'/', disabled:false, sts:30},
-        {id:2, title:'社員マスタ', link:'/employees', disabled:false, sts:50},
-        {id:3, title:'スキルマスタ', link:'＃', disabled:true, sts:10},
-        {id:4, title:'サービスマスタ', link:'＃', disabled:true, sts:10},
-        {id:5, title:'資格マスタ', link:'＃', disabled:true, sts:0},
-        {id:6, title:'保有スキル', link:'＃', disabled:true, sts:10}
-      ],
       max:100
     }
   },
@@ -129,7 +109,7 @@ html, {
 .wrapper > * {
   /* padding: 10px; */
 }
-.menu-bar {
+.left-side {
   order: 0;
   width: 250px;
   padding: 15px;
