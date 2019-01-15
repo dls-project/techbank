1186181008f8806dc77fe0c186f9e6255ec048a8
張松園 <s.cho.seraku@gmail.com>
2019-01-16 08:09:26 +0900

    nuxt

---
+++ nuxt/nuxt.config.js
@@ -0,0 +1,89 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'spa',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+  ** Customize the progress-bar colors
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [],
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: {
+            url: 'login',
+            method: 'post',
+            propertyName: 'meta.token'
+          },
+          user: { url: 'user', method: 'get', propertyName: 'data' },
+          logout: { url: 'logout', method: 'post' }
+        }
+      }
+    }
+  },
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios',
+    // https://auth.nuxtjs.org/
+    '@nuxtjs/auth',
+    // Doc: https://bootstrap-vue.js.org/docs/s
+    'bootstrap-vue/nuxt'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+    baseURL: 'http://localhost:8000/api'
+  },
+
+  router: {
+    middleware: ['auth']
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    {{ $auth.loggedIn }}
+    {{ $auth.user }}
+    {{ $auth.user.name }}
+    {{ $auth.user.email }}
+    <button @click="logout">
+      logout
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {
+    logout() {
+      this.$auth.logout()
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
