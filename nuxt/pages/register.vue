bf57cddee98644590404099ceef37db6187e6213
Hokko4 <horikoshi@seraku.co.jp>
2019-02-19 17:08:01 +0900

    見た目途中

--- nuxt/pages/register.vue
+++ nuxt/pages/register.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container 
     fruid 
-    style="margin-top: 20px;">
+    style="margin: 20px auto;">
     <b-form @submit.prevent="register">
       <b-row>
         <b-col md="6">
@@ -9,10 +9,10 @@
             id="employeeEmail" 
             label="メールアドレス" 
             label-for="email" 
-            description="あなたのメールアドレスです">
+            description="あなたのメールアドレスが表示されています">
             <b-form-input 
               id="email"
-              :value="this.$auth.user.email || 'not login'" 
+              :value="this.$data.form.email"
               disabled />
           </b-form-group>
         </b-col>
@@ -33,6 +33,43 @@
           </b-form-group>
         </b-col>
       </b-form-row>
+      <b-form-row>
+        <b-col md="6">
+          <b-form-group>
+            <b-form-select 
+              v-model="form.services" 
+              :options="form.services_options">
+              <template slot="first">
+                <option 
+                  :value="null" 
+                  disabled>領域を選択してください</option>
+              </template>
+            </b-form-select>
+          </b-form-group>
+        </b-col>
+      </b-form-row>
+      <b-form-row>
+        <b-col md="6">
+          <b-form-group label="資格">
+            <b-form-checkbox-group 
+              v-model="form.qualifications"
+              :options="form.qualifications_options" 
+              stacked 
+              name="資格"/>
+          </b-form-group>
+        </b-col>
+      </b-form-row>
+      <b-form-row>
+        <b-col md="6">
+          <b-form-group label="スキル">
+            <b-form-checkbox-group 
+              v-model="form.skills"
+              :options="form.skills_options"
+              stacked 
+              name="スキル"/>
+          </b-form-group>
+        </b-col>
+      </b-form-row>
       <b-button 
         variant="success" 
         type="submit">登録</b-button>
@@ -43,11 +80,31 @@
 <script>
 export default {
   layout: 'auth',
-  data() {
+  // ログインしていなくても表示できるように
+  auth: false,
+  asyncData() {
     return {
       form: {
-        email: 'test@test.com',
-        station: ''
+        email: 'your@email.com',
+        station: '',
+        services: null,
+        services_options: [
+          { text: '領域１', value: '領域１' },
+          { text: '領域２', value: '領域２' },
+          { text: '領域３', value: '領域３' }
+        ],
+        qualifications: [],
+        qualifications_options: [
+          { text: '資格１', value: '資格１' },
+          { text: '資格２', value: '資格２' },
+          { text: '資格３', value: '資格３' }
+        ],
+        skills: [],
+        skills_options: [
+          { text: 'スキル１', value: 'スキル１' },
+          { text: 'スキル２', value: 'スキル２' },
+          { text: 'スキル３', value: 'スキル３' }
+        ]
       }
     }
   },
